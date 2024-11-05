9df259b23dc205eda446df569ce01aef454988e8
hannahomeiza <omeiza81@gmail.com>
2024-11-04 21:00:00 -0500

    conflicats

--- PP1/scriptorium/pages/api/blog/[id]/template/[templateId].js
+++ PP1/scriptorium/pages/api/blog/[id]/template/[templateId].js
@@ -3,7 +3,7 @@ import { authMiddleware } from "@/lib/auth";
 
 async function handlerDelete(req, res) {
     if (req.method !== "DELETE") {
-        return res.status(405).json({ message: "Method not allowed" });
+        return res.status(405).json({ error: "Method not allowed" });
     }
 
     const blogId = parseInt(req.query.id);
@@ -11,10 +11,30 @@ async function handlerDelete(req, res) {
 
     // Validate IDs
     if (isNaN(blogId) || isNaN(templateId)) {
-        return res.status(400).json({ message: "Invalid blog or template ID" });
+        return res.status(400).json({ error: "Invalid blog or template ID" });
     }
 
     try {
+        const author = await authMiddleware(req, res, { getFullUser: true });
+        if (!author) {
+            return res.status(403).json({ error: "Unauthorized. Please log in to create code." });
+        }
+
+        const blog = await prisma.blog.findUnique({
+            where: { id: blogId }
+            });
+    
+        if (!blog) {
+            return res.status(404).json({ error: "Blog not found" })
+        }
+
+        const isAuthor = blog.authorId === author.id;
+        const isAdmin = author.role === "SYS_ADMIN";
+
+        if(!isAuthor && !isAdmin){
+            return res.status(403).json({ error: "Unauthorized. This is not your blog" });
+        }
+
         // Fetch the blog along with its templatesW
         const template = await prisma.template.findUnique({
             where: { id: templateId }
@@ -37,10 +57,11 @@ async function handlerDelete(req, res) {
         });
 
         // Return the found template
-        return res.status(200).json(updatedBlog);
+        return res.status(200).json({message: "Success!", changedBlog: updatedBlog});
 
     } catch (error) {
-        return res.status(422).json({ error: error.message });
+        console.log("failed to delete template: ", error)
+        return res.status(422).json({ error: "failed to delete template"});
     }
 }
 
@@ -54,11 +75,33 @@ async function handlerUpdate(req, res) {
 
     // Validate IDs
     if (isNaN(blogId) || isNaN(templateId)) {
-        return res.status(400).json({ message: "Invalid blog or template ID" });
+        return res.status(400).json({ error: "Invalid blog or template ID" });
     }
 
     try {
-        // Fetch the blog along with its templatesW
+
+        const author = await authMiddleware(req, res, { getFullUser: true });
+        if (!author) {
+            return res.status(403).json({ error: "Unauthorized. Please log in to create code." });
+        }
+
+        const blog = await prisma.blog.findUnique({
+            where: { id: blogId }
+            });
+    
+        if (!blog) {
+            return res.status(404).json({ error: "Blog not found" })
+        }
+
+        const isAuthor = blog.authorId == author.id;
+        const isAdmin = author.role == "SYS_ADMIN";
+
+        if(!isAuthor && !isAdmin){
+            return res.status(403).json({ error: "Unauthorized. This is not your blog" });
+        }
+
+
+        // Fetch the blog along with its templates
         const template = await prisma.template.findUnique({
         where: { id: templateId }
         });
@@ -79,22 +122,20 @@ async function handlerUpdate(req, res) {
         },
         });
 
-        // Return the found template
-        return res.status(200).json(updatedBlog);
+        // Return the updated blog with templates
+        return res.status(200).json({message: "Success!", changedBlog: updatedBlog});
 
     } catch (error) {
-        return res.status(422).json({ error: error.message });
+        console.log("failed to update template: ", error)
+        return res.status(422).json({ error: "failed to update template"});
     }
 }
 
 export default async function handler(req, res) {
-    const author = await authMiddleware(req, res);
-    if (!author) {
-        return res.status(401).json({ message: "Unauthorized. Please log in to create code." });
-    }
 
     switch(req.method) {
-        case "DELETE": // MAKE SURE USER IS OWNER OR ADMIN
+        // MAKE SURE USER IS OWNER OR ADMIN, doneee
+        case "DELETE": 
             await handlerDelete(req, res);
             return;
 
